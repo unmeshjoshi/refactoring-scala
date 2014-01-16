ba03dfc2331f480b12c67e9683c914090d61865d
unmeshjoshi <unmeshjoshi@gmail.com>
2014-01-16 17:49:01 +0530

    Updated query building method to use list

--- src/main/scala/refactor/IntroduceNamedParameters.scala
+++ src/main/scala/refactor/IntroduceNamedParameters.scala
@@ -1,22 +1,25 @@
 package refactor
 
+/**
+ * named parameters are by default supported since Scala 2.8. So no need to pass Hash like in Ruby. #see IntroduceNamedParametersTest.
+ */
 object IntroduceNamedParameters {
 
   case class SearchCriteria(authorId: String, publisherId: String, isbn: String)
 
   object BooksQuery {
-    def build(selector: String, conditions: Option[String] = None, joins: Seq[String] = Seq()): String = {
-      var sql = "SELECT * FROM books "
-      joins.foreach({
+    def build(selector: String, conditions: Option[String] = None, joins: Seq[String] = Seq()) = {
+      var sql = Seq("SELECT * FROM books ")
+      joins.foreach {
         joinTable =>
-          sql += s"LEFT OUTER JOIN $joinTable ON "
-          sql += s"books.$joinTable" + "_id "
-          sql += s" = $joinTable.id "
-      })
-      if (conditions.isDefined) sql += s" WHERE $conditions"
-      if (selector == "first") sql += " LIMIT 1"
+          sql :+= s"LEFT OUTER JOIN $joinTable ON "
+          sql :+= s"books.$joinTable" + "_id "
+          sql :+= s" = $joinTable.id "
+      }
+      if (conditions.isDefined) sql :+= s" WHERE $conditions"
+      if (selector == "first") sql :+= " LIMIT 1"
 
-      sql
+      sql.mkString
     }
   }
 
--- src/main/scala/refactor/ReplaceTempWithQuery.scala
+++ src/main/scala/refactor/ReplaceTempWithQuery.scala
@@ -1,18 +1,23 @@
 package refactor
 
-class ReplaceTempWithQuery(quantity:Int, itemPrice:Double) {
+case class ReplaceTempWithQuery(quantity: Int, itemPrice: Double) {
 
-  def price:Double = {
-    val basePrice = quantity * itemPrice;
-    var discountFactor = 0.0
-    if (basePrice > 1000) discountFactor = 0.98 else discountFactor = 0.95
-    return basePrice * discountFactor
+  object Original {
+    def price: Double = {
+      val basePrice = quantity * itemPrice;
+      var discountFactor = 0.0
+      if (basePrice > 1000) discountFactor = 0.98 else discountFactor = 0.95
+      return basePrice * discountFactor
+    }
   }
 
 
-  //refactored methods
-  def priceRefactored = basePrice * discountFactor
+  object Refactored {
+    def priceRefactored = basePrice * discountFactor
 
-  def discountFactor = if (basePrice > 1000) 0.98 else 0.95
-  def basePrice = quantity * itemPrice
+    def discountFactor = if (basePrice > 1000) 0.98 else 0.95
+
+    def basePrice = quantity * itemPrice
+  }
+
 }
