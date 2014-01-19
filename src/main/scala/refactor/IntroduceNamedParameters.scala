34718edc647b845081a764e99edca27c2d592038
Mushtaq Ahmed <mushtaq.a@gmail.com>
2014-01-19 13:35:25 +0530

    immutable booksQuery. Test was wrong as well.

--- src/main/scala/refactor/IntroduceNamedParameters.scala
+++ src/main/scala/refactor/IntroduceNamedParameters.scala
@@ -8,7 +8,7 @@ object IntroduceNamedParameters {
   case class SearchCriteria(authorId: String, publisherId: String, isbn: String)
 
   object BooksQuery {
-    def build(selector: String, conditions: Option[String] = None, joins: Seq[String] = Seq()) = {
+    def build1(selector: String, conditions: Option[String] = None, joins: Seq[String] = Seq()) = {
       var sql = Seq("SELECT * FROM books ")
       joins.foreach {
         joinTable =>
@@ -21,6 +21,19 @@ object IntroduceNamedParameters {
 
       sql.mkString
     }
-  }
 
+    def build(selector: String, conditions: Option[String] = None, joins: Seq[String] = Seq()) = {
+
+      val baseQuery = "SELECT * FROM books "
+
+      val joinsStr = joins.map { joinTable =>
+        s"LEFT OUTER JOIN $joinTable ON books.${joinTable}_id  = $joinTable.id "
+      }.mkString(" ")
+
+      val conditionsStr = conditions.map(x => s" WHERE $x").getOrElse("")
+      val selectorStr = if(selector == "first") " LIMIT 1" else ""
+
+      baseQuery + joinsStr + conditionsStr + selectorStr
+    }
+  }
 }
--- src/test/scala/refactor/IntroduceNamedParametersTest.scala
+++ src/test/scala/refactor/IntroduceNamedParametersTest.scala
@@ -1,8 +1,9 @@
 package refactor
 
 import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
 
-class IntroduceNamedParametersTest extends FlatSpec {
+class IntroduceNamedParametersTest extends FlatSpec with ShouldMatchers {
 
   it should "pass named parameters" in {
     val criteria = new IntroduceNamedParameters.SearchCriteria(authorId = "Author", publisherId = "publisherId", isbn = "isbn")
@@ -13,8 +14,7 @@ class IntroduceNamedParametersTest extends FlatSpec {
 
   it should "pass named parameters to build query" in {
     val sql = IntroduceNamedParameters.BooksQuery.build("first", conditions=Option("authors.name='Jenny James'"), joins=Seq("authors"))
-    assert("SELECT * FROM books LEFT OUTER JOIN authors ON books.authors_id  = authors.id  WHERE Some(authors.name='Jenny James') LIMIT 1"
-      == sql)
+    sql should be("SELECT * FROM books LEFT OUTER JOIN authors ON books.authors_id  = authors.id  WHERE authors.name='Jenny James' LIMIT 1")
   }
 
 }
